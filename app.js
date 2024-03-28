f482715ba6cea0a8bc6c9573d612c2be3fd184b5
Paul Ejiofor <ejioforpaul@yahoo.com>
2024-03-28 12:44:58 -0400

    JavaScript Commit

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+const firstName = undefined;
+let lastName;
+var age;
+
 
 // Exercise 2
+firstName = "David";
+lastName = "Micheal";
+age = 12;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1999;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Ruthfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
